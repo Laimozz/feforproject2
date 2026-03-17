88cd5c265cd8b3e183f0b04e1096d8f4c80c4079
Liam <longlb252004@gmail.com>
2026-03-17 19:39:34 +0700

    Chuan bi deploy

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://be-u9j0.onrender.com/api";
 
 export const ENDPOINTS = {
   // Auth
